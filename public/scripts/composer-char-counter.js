726f068734a902e1b652351754e1b8313117417c
Lanuvelza <ianlam1221@hotmail.com>
2021-01-26 19:59:10 +0000

    event handler for updating character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // --- our code goes here ---  
+  const num = $(".counter").val(); 
+  $("#tweet-text").on('keyup', function() {
+    let string = $(this).val(); 
+    $(".counter").val(num - string.length);
+    let count = num - string.length;
+    if (count <= 0) {
+      $(".counter").css("color", "red")
+    } else {
+      $(".counter").css("color", "#545149");
+    }
+  });
+
+
+});
