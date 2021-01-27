f656dedb66f8297fdd196447bdb2e5d4762a85c7
Lanuvelza <ianlam1221@hotmail.com>
2021-01-27 04:36:05 +0000

    refactored using this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-  // --- our code goes here ---  
-  const num = $(".counter").val(); 
-  $("#tweet-text").on('keyup', function() {
-    let string = $(this).val(); 
-    $(".counter").val(num - string.length);
+  // --- our code goes here ---
+  const num = $(".counter").val();
+
+  $("#post-tweet").on('keyup', function() {
+    let string = $(this.children[0]).val();
+    const counter = $(this.children[1].children[1]);
+    counter.val(num - string.length);
     let count = num - string.length;
     if (count <= 0) {
-      $(".counter").css("color", "red")
+      counter.css("color", "red");
     } else {
-      $(".counter").css("color", "#545149");
+      counter.css("color", "#545149");
     }
   });
+
 });
